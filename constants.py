48d303056e0c6149ebc44d56ff65c2269d8be2f4
Jack Cherng <jfcherng@gmail.com>
2022-07-05 00:08:53 +0800

    refactor: mark implemented request/notification
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- constants.py
+++ constants.py
@@ -4,18 +4,25 @@ PACKAGE_VERSION = "0.0.1"
 COPILOT_WAITING_COMPLETION_KEY = "copilot.completion.is_waiting"
 PHANTOM_KEY = "copilot.completion.previews"
 
-REQ_INITIALIZE = "initialize"
-REQ_SET_EDITOR_INFO = "setEditorInfo"
-REQ_CHECK_STATUS = "checkStatus"
-REQ_SIGN_IN_INITIATE = "signInInitiate"
-REQ_SIGN_IN_CONFIRM = "signInConfirm"
-REQ_SIGN_OUT = "signOut"
-REQ_GET_COMPLETIONS = "getCompletions"
-REQ_RECORD_TELEMETRY_CONSENT = "recordTelemetryConsent"
-REQ_GET_COMPLETIONS_CYCLING = "getCompletionsCycling"
-REQ_NOTIFY_SHOWN = "notifyShown"
-REQ_NOTIFY_REJECTED = "notifyRejected"
-REQ_NOTIFY_ACCEPTED = "notifyAccepted"
+# ---------------- #
+# Copilot requests #
+# ---------------- #
 
-NTFY_STATUS_NOTIFICATION = "statusNotification"
-NTFY_LOG_MESSAGE = "LogMessage"
+REQ_CHECK_STATUS = "checkStatus"  # done
+REQ_GET_COMPLETIONS = "getCompletions"  # done
+REQ_GET_COMPLETIONS_CYCLING = "getCompletionsCycling"
+REQ_NOTIFY_ACCEPTED = "notifyAccepted"
+REQ_NOTIFY_REJECTED = "notifyRejected"
+REQ_NOTIFY_SHOWN = "notifyShown"
+REQ_RECORD_TELEMETRY_CONSENT = "recordTelemetryConsent"
+REQ_SET_EDITOR_INFO = "setEditorInfo"  # done
+REQ_SIGN_IN_CONFIRM = "signInConfirm"  # done
+REQ_SIGN_IN_INITIATE = "signInInitiate"  # done
+REQ_SIGN_OUT = "signOut"  # done
+
+# --------------------- #
+# Copilot notifications #
+# --------------------- #
+
+NTFY_LOG_MESSAGE = "LogMessage"  # done
+NTFY_STATUS_NOTIFICATION = "statusNotification"  # done
