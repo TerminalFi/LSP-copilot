72e07aa773b21b12c504294b957de89d42442d0f
Jack Cherng <jfcherng@gmail.com>
2022-07-06 02:31:02 +0800

    fix: get_project_relative_path() wrong logic
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- utils.py
+++ utils.py
@@ -32,10 +32,10 @@ def update_completion_preview(
 
 
 def get_project_relative_path(file_path: str) -> str:
-    ret = ""
+    ret = file_path
     for folder in sublime.active_window().folders():
         try:
             ret = min(ret, os.path.relpath(file_path, folder), key=len)
         except ValueError:
             pass
-    return ret or file_path
+    return ret
