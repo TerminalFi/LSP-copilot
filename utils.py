96c960eebb3c87f6711bd49e619a9c9363c44321
Jack Cherng <jfcherng@gmail.com>
2022-07-18 02:01:50 +0800

    refactor: some renamings
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- utils.py
+++ utils.py
@@ -32,14 +32,14 @@ def erase_copilot_view_setting(view: sublime.View, key: str) -> None:
     view.settings().erase("{}.{}".format(COPILOT_VIEW_SETTINGS_PREFIX, key))
 
 
-def get_project_relative_path(file_path: str) -> str:
-    ret = file_path
+def get_project_relative_path(path: str) -> str:
+    relpath = path
     for folder in sublime.active_window().folders():
         try:
-            ret = min(ret, os.path.relpath(file_path, folder), key=len)
+            relpath = min(relpath, os.path.relpath(path, folder), key=len)
         except ValueError:
             pass
-    return ret
+    return relpath
 
 
 def get_setting(session: Session, key: str, default: Optional[Union[str, bool, List[str]]] = None) -> Any:
